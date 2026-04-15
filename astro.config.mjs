2d2b0553ec4938ef458661d877af3aca64cc2788
gonwms <gon.williams@gmail.com>
2026-04-14 22:21:30 -0300

    feat: initialize project structure with Astro, Tailwind CSS, and core UI components

---
+++ astro.config.mjs
@@ -0,0 +1,41 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
+import tailwindcss from "@tailwindcss/vite";
+
+// Plugin para eliminar comentarios HTML
+const removeHtmlComments = {
+  name: "remove-html-comments",
+  apply: /** @type {const} */ ("build"),
+  enforce: /** @type {const} */ ("post"),
+  // @ts-ignore
+  transformIndexHtml(html) {
+    if (typeof html === "string") {
+      return html.replace(/<!--[\s\S]*?-->/g, "");
+    }
+    return html;
+  }
+};
+
+let DEPLOY_DOMAIN = "https://gonwms.github.io";
+let DEPLOY_PATH = "/red-terapia/";
+
+// UNCOMMENT FOR CUSTOM DOMAIN:
+// DEPLOY_DOMAIN = "https://redterapia.com";
+// DEPLOY_PATH = "/";
+
+export default defineConfig({
+  site: DEPLOY_DOMAIN,
+  base: DEPLOY_PATH,
+  build: { assets: "assets" },
+  integrations: [
+    icon({
+      include: {
+        "simple-icons": ["*"],
+      },
+    }),
+  ],
+  vite: {
+    plugins: [tailwindcss(), removeHtmlComments],
+  },
+});
